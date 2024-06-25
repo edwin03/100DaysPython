d965597ac17c53b5dafb7aa3c5ca3c22e42ea3ab
Edwin <edwin.rubio@toshiba.com>
2024-06-25 17:08:41 -0500

    Pushing data to git to continue on another computer.

--- Day1/day-1-printing-start.py
+++ Day1/day-1-printing-start.py
@@ -1,2 +1,3 @@
-# Write your code below this line 👇
+# Write your code below this line 👇. By the way this is comment that is not exectued.
 print("Hello World!" + " Edwin")
+print("Hello " + input("What is your name?"))
