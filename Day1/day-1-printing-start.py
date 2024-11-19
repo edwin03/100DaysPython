7a4be28aaeec1f0d1c175f0c95622fb1798dcd7d
Edwin Rubio <edwin.rubio@ymail.com>
2024-11-19 09:17:52 -0600

    modifed day 1 printing start to include exlamation mark

--- Day1/day-1-printing-start.py
+++ Day1/day-1-printing-start.py
@@ -1,3 +1,3 @@
 # Write your code below this line 👇. By the way this is comment that is not exectued.
 print("Hello World!" + " Edwin")
-print("Hello " + input("What is your name?"))
+print("Hello " + input("What is your name? ") + "!")
