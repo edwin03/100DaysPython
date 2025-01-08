f2a6b214545551a674874517f16bf0ded7e901f2
Edwin <edwin.rubio@toshiba.com>
2025-01-08 16:38:13 -0600

    add: added day 5 content based on the videos on 01-08-25.

---
+++ Day5/day-5-start.py
@@ -0,0 +1,36 @@
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+	print(fruit)
+
+stu_scores = [100,1,2,3,101]
+total_exam_score = sum(stu_scores)
+print(f"This is the sum {total_exam_score}")
+
+summ = 0
+for score in stu_scores:
+	summ += score
+
+print(f"sum is {summ}")
+
+#Find the max number
+maxs = max(stu_scores)
+print(f"This is the maxs {maxs}")
+
+highest = 0
+for score in stu_scores:
+	if score > highest:
+		highest = score
+print(f"highest number is {highest}")
+
+for number in range(1,10):# this does not print the 10
+	print(number)
+
+# This is for stepping. The last parameter is the stepper.
+for number in range(1,10, 2):# this does not print the 10
+	print(number)
+
+ #Gauss Challange
+summer = 0
+for num in range(1, 101):
+	summer += num
+print(summer)
