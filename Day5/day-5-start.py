e06ce6487fc5e7023803ec49b39ed3428160f63f
Edwin Rubio <edwin.rubio@ymail.com>
2025-01-10 14:31:23 -0600

    add: finalized day 5 project on 01-10-25.

--- Day5/day-5-start.py
+++ Day5/day-5-start.py
@@ -34,3 +34,15 @@ summer = 0
 for num in range(1, 101):
 	summer += num
 print(summer)
+
+#FizzBuzz game
+
+for num in range(1, 101):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
