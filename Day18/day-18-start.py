07225cdd0cc355672d31b44297b6278db041d4fd
Edwin Rubio <edwin.rubio@ymail.com>
2025-03-03 07:17:14 -0600

    add: added more day 18 stuff on 03-03-25.

--- Day18/day-18-start.py
+++ Day18/day-18-start.py
@@ -1,13 +1,34 @@
 from turtle import Turtle, Screen
+import heroes
+import villains
+from random import randint
+
+print(heroes.gen())
+print(villains.gen())
 
 tim = Turtle()
 tim.shape("turtle")
 tim.color("OliveDrab4")
 
-for _ in range(0,4):
-    tim.forward(100)
-    tim.right(90)
+# for _ in range(4):
+#     tim.forward(100)
+#     tim.right(90)
 
+
+# for _ in range(15):
+#     tim.pendown()
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
 
 screen = Screen()
+screen.colormode(255)
+
+for sides in range(3, 11):
+    angle = 360/sides
+    tim.pencolor((randint(0, 255), randint(0, 255), randint(0, 255)))
+    for _ in range(1, sides+1):
+        tim.right(angle)
+        tim.forward(100)
+    
 screen.exitonclick()
